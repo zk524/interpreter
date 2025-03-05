ee51b042791cce1a1ccf8f7e8c7b68a44babb36f
zk524 <zk524gg@gmail.com>
2025-03-05 23:42:38 +0800

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ init().then(() => {
                     await sync_state()
                     self.postMessage({ type: "synced", mesg: `Synced!` })
                 } catch (e) {
-                    self.postMessage({ type: 'synced', mesg: `Error: ${e.toString()}` })
+                    self.postMessage({ type: 'mesg', mesg: `Error: ${e.toString()}` })
                 }
                 break
             case "read":
@@ -18,7 +18,7 @@ init().then(() => {
                     const count = counter()
                     self.postMessage({ type: "read", mesg: `Count: ${count}` })
                 } catch {
-                    self.postMessage({ type: "read", mesg: `Read failed.` })
+                    self.postMessage({ type: "mesg", mesg: `Read failed.` })
                 }
                 break
             case "prov":
@@ -28,7 +28,7 @@ init().then(() => {
                     const end = performance.now()
                     self.postMessage({ type: "prov", tx, mesg: `Proved! Cost time: ${(end - start).toFixed(4)} ms` })
                 } catch {
-                    self.postMessage({ type: "prov", tx: "", mesg: `Prove failed.` })
+                    self.postMessage({ type: "mesg", tx: "", mesg: `Prove failed.` })
                 }
                 break
             case "send":
@@ -36,14 +36,14 @@ init().then(() => {
                     await send_tx(e.data.tx)
                     self.postMessage({ type: "sent", mesg: `TX has been sent.` })
                 } catch {
-                    self.postMessage({ type: "sent", mesg: `Sent failed.` })
+                    self.postMessage({ type: "mesg", mesg: `Sent failed.` })
                 }
                 break
             case "dump":
                 try {
                     self.postMessage({ type: "dump", data: dump_state() })
                 } catch {
-                    self.postMessage({ type: "dump", mesg: `Dump failed.` })
+                    self.postMessage({ type: "mesg", mesg: `Dump failed.` })
                 }
                 break
             case "rest":
@@ -51,7 +51,7 @@ init().then(() => {
                     rest_state(e.data.data)
                     self.postMessage({ type: "rest", mesg: "State has been rest." })
                 } catch {
-                    self.postMessage({ type: "rest", mesg: `Rest failed.` })
+                    self.postMessage({ type: "mesg", mesg: `Rest failed.` })
                 }
                 break
         }
